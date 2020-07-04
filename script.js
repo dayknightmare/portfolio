b96e88485f1d3c55e69d4376e1c1398b0083db9d
miguel <miguelhunter95@gmail.com>
2020-07-04 16:22:11 -0300

    Port

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const app = new Vue({
     el: '.app',
     data: {
         accord: '',
-        active: ''
+        active: '',
     },
 
     created() {
@@ -12,32 +12,45 @@ const app = new Vue({
     methods: {
         openAccord(id) {
             let acs = document.querySelectorAll('div.accordItem');
+            let buttons = document.querySelectorAll('button.accord')
 
             for (let i of acs){
                 if (i.id == this.accord){
                     i.classList.remove('active');
                     i.classList.add('indel');
+
                     setTimeout(function () {
                         i.classList.remove('indel');
-                    }, 301)
+                    }, 601)
 
                 }
             }
+
+            for (let i of buttons){
+                i.classList.remove('halfboard')
+            }
+
             if (id != this.accord){
                 this.accord = id
                 document.querySelector(`#${id}`).classList.add('inopen')
+                document.querySelector(`#${id}-b`).classList.add('halfboard')
+
                 setTimeout(function () {
 
                     document.querySelector(`#${id}`).classList.add('active')
                 }, 10)
+
                 setTimeout(function () {
                     document.querySelector(`#${id}`).classList.remove('inopen')
 
-                }, 301)
+                }, 601)
 
             }
+
             else{
                 this.accord = ''
+                document.querySelector(`#${id}-b`).classList.remove('halfboard')
+
             }
         },
 
@@ -49,14 +62,28 @@ const app = new Vue({
             if (window.pageYOffset < iknow.offsetTop - 150){
                 this.active = "orewa"
             }
+
             else if (window.pageYOffset < projects.offsetTop - 150){
                 this.active = "iknow"
             }
+
             else if (window.pageYOffset < moshi.offsetTop - 650){
                 this.active = "projects"
             }
+
             else{
                 this.active = "moshi"
+            }
+
+            let p = document.querySelectorAll('div.hopeproject')
+
+            for (i of p){
+                if (i.offsetTop - 400 <= window.pageYOffset){
+                    i.classList.add('view')
+                }
+                else{
+                    i.classList.remove('view') 
+                }
             }
         },
 
