963c254d08fe55b19a5203eab271d10e74e41c9d
Miguel Vieira Colombo <miguelhunter95@gmail.com>
2022-08-03 21:47:18 -0300

    Deploy gh-pages

---
+++ _next/static/xCL6G-9itobKMQCZLD18_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-594a3108d28717e2.js","static/chunks/pages/index-9d7aa29e1c214fb9.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
