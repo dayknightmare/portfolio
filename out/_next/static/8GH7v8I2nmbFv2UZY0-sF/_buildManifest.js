f5aabd087ea4cf4f10efe7d1ae7655eaeefc116c
Miguel Vieira Colombo <miguelhunter95@gmail.com>
2023-10-22 20:59:25 -0300

    Fix path images

---
+++ out/_next/static/8GH7v8I2nmbFv2UZY0-sF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-38c251d3ee779048.js","static/chunks/pages/index-f35c4bc276466744.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
