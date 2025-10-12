fa07f57b443f1c44080871c39c78f8400451a13f
Miguel Colombo <miguelhunter95@gmail.com>
2025-10-12 14:24:23 -0300

    fix modal animation

--- src/components/projects/Modal.tsx
+++ src/components/projects/Modal.tsx
@@ -1,8 +1,7 @@
 'use client';
 
+import { AnimatePresence, motion } from 'motion/react';
 import { MouseEvent, useEffect, useState } from 'react';
-
-import { motion } from 'motion/react';
 
 type ImageModalProps = {
   imgs: string[];
@@ -37,39 +36,35 @@ export const Modal = ({ imgs, active, onClose }: ImageModalProps) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [active]);
 
-  return <motion.div
-    className="image-modal has_close"
-    onClick={(e) => closeModal(e)}
-    animate={{
-      opacity: active ? 1 : 0,
-      display: active ? 'flex' : 'none',
-    }}
-    transition={{ duration: .3 }}
-  >
-    <div
-      className="
-        rounded-md
-        modal-hero
-        modal-content
-        delay-150
-        transition-all
-        ease-in-out
-        duration-300
-        flex
-        justify-center
-        items-center
-        z-10
-        has_close
-        hero-image
-    ">
-      <div className="has_close rounded-full bg-white px-4 py-2 cursor-pointer absolute right-10 top-10 select-none">Close</div>
-      <div className="controls rounded-full bg-white px-4 py-2 cursor-pointer absolute left-10 select-none" onClick={() => setIndex(index > 0 ? index - 1 : imgs.length - 1)}>{'<'} Prev</div>
-      <div className="controls rounded-full bg-white px-4 py-2 cursor-pointer absolute right-10 select-none" onClick={() => setIndex(index < imgs.length - 1 ? index + 1 : 0)}>Next {'>'}</div>
-      {
-        imgs[index] != '' &&
-          // eslint-disable-next-line @next/next/no-img-element
-          <img className="max-w-full max-h-full h-auto w-auto rounded-lg" src={imgs[index]} alt="Modal image" />
-      }
-    </div>
-  </motion.div>;
+  return <AnimatePresence initial={false}>
+    {
+      active ? (
+        <motion.div
+          className="modal-image has_close "
+          onClick={(e) => closeModal(e)}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div
+            className="
+              rounded-md
+              flex
+              justify-center
+              items-center
+              has_close
+              modal-content
+          ">
+            <div className="has_close rounded-full bg-white px-4 py-2 cursor-pointer absolute right-10 top-10 select-none">Close</div>
+            <div className="controls rounded-full bg-white px-4 py-2 cursor-pointer absolute left-10 select-none" onClick={() => setIndex(index > 0 ? index - 1 : imgs.length - 1)}>{'<'} Prev</div>
+            <div className="controls rounded-full bg-white px-4 py-2 cursor-pointer absolute right-10 select-none" onClick={() => setIndex(index < imgs.length - 1 ? index + 1 : 0)}>Next {'>'}</div>
+            {
+              imgs[index] != '' &&
+              // eslint-disable-next-line @next/next/no-img-element
+              <img className="max-w-full max-h-full h-auto w-auto rounded-lg" src={imgs[index]} alt="Modal image" />
+            }
+          </div>
+        </motion.div> ) : null
+    }
+  </AnimatePresence>;
 };
